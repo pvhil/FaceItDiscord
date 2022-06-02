cf002c9a491f4171e8c2d0a7b731e9ae45e2907b
JustDams <ddw_github@de-weerd.name>
2022-06-02 23:01:05 +0200

    :art: format

---
+++ commands/team.js
@@ -0,0 +1,15 @@
+const { fTeam } = require('../embedFinisher')
+const { getInteractionOption } = require('../utils/interaction')
+const { postCommandToStatcord } = require('../utils/statcord')
+
+module.exports = async interaction => {
+  await interaction.deferReply()
+  var team = getInteractionOption(interaction, 'team')
+  var resp = await fTeam(team)
+  
+  postCommandToStatcord(interaction)
+
+  interaction.editReply({
+    embeds: [resp]
+  })
+}
