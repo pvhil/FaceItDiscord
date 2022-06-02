76082de8bc52dfa6c1b3267a31596823869d0340
JustDams <ddw_github@de-weerd.name>
2022-06-03 00:08:54 +0200

    :recycle: refactor embeds

--- embedFinisher.js
+++ embedFinisher.js
@@ -1,40 +1,45 @@
-const fr = require('./faceitRequests')
+const { countryCodeEmoji } = require('country-code-emoji')
+const { MessageEmbed } = require('discord.js')
 const {
-  countryCodeEmoji,
-} = require('country-code-emoji')
+  nickStats,
+  searchPlayer,
+  historyReq,
+  detailedMatch,
+  idStatsCheck,
+  idStats,
+  plRankRegion,
+  plRankCountry,
+  vOneRequest,
+  rankRegion,
+  rankCountry,
+  searchHub,
+  hubInfo,
+  hubLeaderboard,
+  teamSearch,
+  teamInfo
+} = require('./faceitRequests')
+
 const numbformat = new Intl.NumberFormat()
 
-const {
-  MessageEmbed,
-} = require('discord.js')
+const checkPlayer = async (name) => {
+  let sCon
+  try { sCon = await nickStats(name) }
+  catch (ex) { sCon = await searchPlayer(name) }
+  return sCon
+}
 
-
-async function fStats(name) {
+const fStats = async name => {
   try {
-    let id
-    try {
-      const sCon = await fr.nickStats(name)
-      id = sCon.player_id
-    } catch (ex) {
-      const sCon = await fr.searchPlayer(name)
-      id = sCon.items[0].player_id
-    }
-
-    var temp = await fr.idStatsCheck(id)
-    var csgoStats = await fr.idStats(temp.player_id)
-    var regiont = await fr.nickStats(temp.nickname)
-    var region = regiont.games.csgo.region
-    var country = regiont.country
-    var rank = await fr.plRankRegion(temp.player_id, region)
-    var rankw = await fr.plRankCountry(temp.player_id, country, region)
-    var skilllevel = regiont.games.csgo['skill_level'].toString()
-
-    var premium = ''
-    if (!(regiont.memberships.toString().includes('free'))) {
-      premium = ' Premium User '
-    }
-
-    var skilllevelpic = 'https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/level' + skilllevel + '.png'
+    const id = (await checkPlayer(name)).player_id,
+      temp = await idStatsCheck(id),
+      csgoStats = await idStats(temp.player_id),
+      region = temp.games.csgo.region,
+      country = temp.country,
+      rank = await plRankRegion(temp.player_id, region),
+      rankw = await plRankCountry(temp.player_id, country, region),
+      skilllevel = temp.games.csgo['skill_level'].toString(),
+      premium = temp.memberships.toString().includes('free') ? '' : ' Premium User ',
+      skilllevelpic = 'https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/level' + skilllevel + '.png'
 
     const embed = new MessageEmbed()
       .setColor('#FF5500')
@@ -68,10 +73,10 @@ async function fStats(name) {
         value: csgoStats.lifetime['Average Headshots %'].toString() + '%',
         inline: true
       })
-      .setAuthor('Elo: ' + regiont.games.csgo['faceit_elo'].toString(), skilllevelpic)
-      .setThumbnail(regiont.avatar)
-      .setDescription('[FaceIT Profile](' + regiont['faceit_url'].toString().replace('{lang}', 'en') + ') and [Steam Profile](https://steamcommunity.com/profiles/' + regiont['steam_id_64'] + ')')
-      .setFooter('🌐 Rank: ' + numbformat.format(rank.position).toString() + ' | Country Rank: ' + numbformat.format(rankw.position).toString())
+      .setAuthor('Elo: ' + temp.games.csgo.faceit_elo.toString(), skilllevelpic)
+      .setThumbnail(temp.avatar)
+      .setDescription(`[FaceIT Profile](${temp.faceit_url.toString().replace('{lang}', 'en')}) and [Steam Profile](https://steamcommunity.com/profiles/${temp.steam_id_64})`)
+      .setFooter(`🌐 Rank: ${numbformat.format(rank.position).toString()} | Country Rank: ${numbformat.format(rankw.position).toString()}`)
 
     return embed
   } catch (e) {
@@ -84,61 +89,37 @@ async function fStats(name) {
     return errembed
   }
 }
-async function fLatest(name) {
+
+const fLatest = async name => {
   try {
-    var id = 0
-    try {
-      const sCon = await fr.nickStats(name)
-      id = sCon.player_id
-    } catch (ex) {
-      const sCon = await fr.searchPlayer(name)
-      id = sCon.items[0].player_id
-    }
-    var historty = await fr.historyReq(id, 1)
-    var history = historty.items[0]
-    var game = await fr.detailedMatch(history['match_id'])
+    const id = (await checkPlayer(name)).player_id,
+      historty = await historyReq(id, 1),
+      history = historty.items[0],
+      game = await detailedMatch(history['match_id'])
 
-    var teamIndex = ''
-    var plIndex = ''
-    var isWin = '0'
+    let teamIndex = ''
+    let plIndex = ''
+    let isWin = '0'
 
-    var team1 = []
-    var team2 = []
+    const team1 = [], team2 = []
 
-    for (var i = 0; i < game.rounds[0].teams.length; i++) {
+    for (let i = 0; i < game.rounds[0].teams.length; i++) {
+      for (let a = 0; a < game.rounds[0].teams[i].players.length; a++)
+        if (i == 0) team1.push(game.rounds[0].teams[i].players[a].nickname)
+        else team2.push(game.rounds[0].teams[i].players[a].nickname)
 
-      for (var a = 0; a < game.rounds[0].teams[i].players.length; a++) {
-        if (i == 0) {
-          team1.push(game.rounds[0].teams[i].players[a].nickname)
-        } else {
-          team2.push(game.rounds[0].teams[i].players[a].nickname)
-        }
-      }
-      if (JSON.stringify(game.rounds[0].teams[i]).includes(id)) {
-
-        for (var a = 0; a < game.rounds[0].teams[i].players.length; a++) {
-
-
+      if (JSON.stringify(game.rounds[0].teams[i]).includes(id))
+        for (let a = 0; a < game.rounds[0].teams[i].players.length; a++)
           if (JSON.stringify(game.rounds[0].teams[i].players[a]).includes(id)) {
             teamIndex = i
             plIndex = a
             isWin = game.rounds[0].teams[i].players[a].Result
-
           }
-        }
-      }
-    }
-    var plStats = game.rounds[0].teams[teamIndex].players[plIndex]['player_stats']
-
-    var date = new Date(historty.to * 1000)
-
-
-    var win = 'https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/l.png'
-
-    if (isWin == '1') {
-      win = 'https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/trophy.png'
     }
 
+    const plStats = game.rounds[0].teams[teamIndex].players[plIndex]['player_stats']
+    let date = new Date(historty.to * 1000)
+    const win = isWin == '1' ? 'https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/trophy.png' : 'https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/l.png'
 
     const embed = new MessageEmbed()
       .setColor('#FF5500')
@@ -157,11 +138,11 @@ async function fLatest(name) {
         inline: false
       }, {
         name: 'Final Score',
-        value: game.rounds[0]['round_stats'].Score.toString(),
+        value: game.rounds[0].round_stats.Score.toString(),
         inline: true
       }, {
         name: 'Map',
-        value: game.rounds[0]['round_stats'].Map.toString(),
+        value: game.rounds[0].round_stats.Map.toString(),
         inline: true
       }, {
         name: '\u200b',
@@ -197,11 +178,11 @@ async function fLatest(name) {
         inline: true
       }, {
         name: 'Assists',
-        value: plStats['Assists'].toString(),
+        value: plStats.Assists.toString(),
         inline: true
       }, {
         name: 'Headshots',
-        value: plStats['Headshots'].toString(),
+        value: plStats.Headshots.toString(),
         inline: true
       }, {
         name: 'Headshot %',
@@ -209,7 +190,7 @@ async function fLatest(name) {
         inline: true
       }, {
         name: 'MVPs',
-        value: plStats['MVPs'].toString(),
+        value: plStats.MVPs.toString(),
         inline: true
       }, {
         name: 'HLTV 1.0',
@@ -217,37 +198,25 @@ async function fLatest(name) {
         inline: true
       })
       .setThumbnail(win)
-      .setDescription('[Link to Game](' + history.faceit_url.toString().replace('{lang}', 'en') + ')')
+      .setDescription(`[Link to Game](${history.faceit_url.toString().replace('{lang}', 'en')})`)
       .setFooter('Match played: ', 'https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/src/main/resources/images/clock.png')
 
     return embed
   } catch (e) {
-    const errembed = new MessageEmbed()
+    return new MessageEmbed()
       .setColor('#ff0000')
       .setTitle('Wrong FaceIT Name')
       .setDescription('This FaceIT Name is invalid! Try again')
       .setTimestamp()
-
-    return errembed
-
   }
 }
 
 async function fMap(name, map = 'de_dust2') {
   try {
-    let id = 0
-
-    try {
-      const sCon = await fr.nickStats(name)
-      id = sCon.player_id
-    } catch (ex) {
-      const sCon = await fr.searchPlayer(name)
-      id = sCon.items[0].player_id
-    }
-
-    const playerStats = await fr.idStats(id)
-    const mapDatas = playerStats.segments.filter(x => x.label == map)[0]
-    const mapStats = mapDatas.stats
+    const id = (await checkPlayer(name)).player_id,
+      playerStats = await idStats(id),
+      mapDatas = playerStats.segments.filter(x => x.label == map)[0],
+      mapStats = mapDatas.stats
 
     const embed = new MessageEmbed()
       .setColor('#FF5500')
@@ -338,34 +307,15 @@ const fLast = async (name, count = 20) => {
     .setTimestamp()
 
   try {
-    let id, nickname
-    try {
-      const sCon = await fr.nickStats(name)
-      id = sCon.player_id
-      nickname = sCon.nickname
-    } catch (ex) {
-      const sCon = await fr.searchPlayer(name)
-      id = sCon.items[0].player_id
-      nickname = sCon.items[0].nickname
-    }
-    const stats = await fr.vOneRequest(id)
-    const nickStats = await fr.nickStats(nickname)
+    const playerDatas = await checkPlayer(name),
+      id = playerDatas.player_id,
+      nickname = playerDatas.nickname,
+      stats = await vOneRequest(id),
+      playerStats = await nickStats(nickname),
+      elopoints = [],
+      kdlist = []
 
-    const elopoints = []
-    const kdlist = []
-    let wins = 0
-    let totalkills = 0
-    let totaldeaths = 0
-    let totalassists = 0
-    let totalmvps = 0
-    let totalhs = 0
-    let totaltriple = 0
-    let totalquadro = 0
-    let totalace = 0
-    let totalrounds = 0
-    let totalkr = 0
-    let totalhsperc = 0
-    let totalkd = 0
+    let wins = 0, totalkills = 0, totaldeaths = 0, totalassists = 0, totalmvps = 0, totalhs = 0, totaltriple = 0, totalquadro = 0, totalace = 0, totalrounds = 0, totalkr = 0, totalhsperc = 0, totalkd = 0
 
     for (let i = 0; i < (count > stats.length ? stats.length : count); i++) {
       const stat = stats[i]
@@ -456,66 +406,64 @@ const fLast = async (name, count = 20) => {
         value: (Math.min(...elopoints) + ' / ' + Math.max(...elopoints)).toString(),
         inline: true
       })
-      .setThumbnail(nickStats.avatar)
-      .setImage('https://quickchart.io/chart?bkg=white&c={type:%27line%27,data:{labels:[' + '"",'.repeat(count) + '],datasets:[{label:%27Elo%27,yAxisID:%27A%27,data:[' + elopoints.toString() + '],backgroundColor:%22rgba(255,0,0,0.5)%22,borderColor:%27red%27},{label:%27K%2FD%27,yAxisID:%27B%27,data:[' + kdlist.toString() + '],backgroundColor:%22rgba(78,80,255,0)%22,borderColor:%27blue%27}]},options:{scales:{xAxes:[{ticks:{reverse:true}}],yAxes:[{id:%27A%27,type:%27linear%27,position:%27left%27,ticks:{beginAtZero:false,}},{id:%27B%27,type:%27linear%27,position:%27right%27,ticks:{beginAtZero:false}}]}}}')
+      .setThumbnail(playerStats.avatar)
+      .setImage(`https://quickchart.io/chart?bkg=white&c={type:%27line%27,data:{labels:[${'"",'.repeat(count)}],datasets:[{label:%27Elo%27,yAxisID:%27A%27,data:[${elopoints.toString()}],backgroundColor:%22rgba(255,0,0,0.5)%22,borderColor:%27red%27},{label:%27K%2FD%27,yAxisID:%27B%27,data:[${kdlist.toString()}],backgroundColor:%22rgba(78,80,255,0)%22,borderColor:%27blue%27}]},options:{scales:{xAxes:[{ticks:{reverse:true}}],yAxes:[{id:%27A%27,type:%27linear%27,position:%27left%27,ticks:{beginAtZero:false,}},{id:%27B%27,type:%27linear%27,position:%27right%27,ticks:{beginAtZero:false}}]}}}`)
 
     return embed
   } catch (e) {
     console.log(e)
+
     return new MessageEmbed()
       .setColor('#ff0000')
       .setTitle('Wrong FaceIT Name')
       .setDescription('This FaceIT Name is invalid! Try again')
       .setTimestamp()
-
   }
 }
 
-async function fRanking(region, country) {
+const fRanking = async (region, country) => {
   try {
-    var ranking = null
+    let ranking
     const embed = new MessageEmbed()
 
     if (country == null) {
-      ranking = await fr.rankRegion(region)
-      embed.setThumbnail('https://www.countryflags.io/' + region + '/flat/64.png')
-      embed.setTitle('Top 15 for ' + region)
+      ranking = await rankRegion(region)
+      embed.setThumbnail(`https://www.countryflags.io/${region}/flat/64.png`)
+        .setTitle(`Top 15 for ${region}`)
     } else {
-      ranking = await fr.rankCountry(country, region)
-      embed.setThumbnail('https://www.countryflags.io/' + country + '/flat/64.png')
-      embed.setTitle('Top 15 for ' + country)
-      console.log(embed.thumbnail)
+      ranking = await rankCountry(country, region)
+      embed.setThumbnail(`https://www.countryflags.io/${country}/flat/64.png`)
+        .setTitle(`Top 15 for ${country}`)
     }
 
-    var ept = []
-    for (var i = 0; i < 15; i++) {
-      var namet = ranking.items[i].nickname
-      var elot = ranking.items[i]['faceit_elo']
-      var lvlt = ranking.items[i]['game_skill_level']
-      ept.push(i + 1 + '. ***' + namet + '***: ' + 'Elo: ' + elot + ' | ' + 'Level: ' + lvlt + '\n')
-
+    const ept = []
+    for (let i = 0; i < 15; i++) {
+      const namet = ranking.items[i].nickname,
+        elot = ranking.items[i].faceit_elo,
+        lvlt = ranking.items[i].game_skill_level
+      ept.push(`${i + 1}. ***${namet}***: Elo: ${elot} | Level: ${lvlt}\n`)
     }
-    embed
-      .setColor('#FF5500')
+
+    embed.setColor('#FF5500')
       .setDescription(ept.join(''))
 
     return embed
   } catch (e) {
     const errembed = new MessageEmbed()
       .setColor('#ff0000')
-      .setTitle('Wrong FaceIT Name')
-      .setDescription('This FaceIT Name is invalid! Try again')
+      .setTitle('Wrong Region and/or Country')
+      .setDescription('The region and/or country are invalid! Try again')
       .setTimestamp()
 
     return errembed
   }
 }
 
-async function fHub(name) {
+const fHub = async name => {
   try {
-    var hubSearch = await fr.searchHub(encodeURIComponent(name))
-    var hubInfo = await fr.hubInfo(hubSearch.items[0]['competition_id'])
-    var hubLeaderboard = await fr.hubLeaderboard(hubSearch.items[0]['competition_id'])
+    const hubSearch = await searchHub(encodeURIComponent(name)),
+      hubInfo = await hubInfo(hubSearch.items[0].competition_id),
+      hubLeaderboard = await hubLeaderboard(hubSearch.items[0].competition_id)
 
     const embed = new MessageEmbed()
       .setColor('#FF5500')
@@ -523,29 +471,29 @@ async function fHub(name) {
       .setDescription(hubInfo['description'])
       .addFields({
         name: 'Players',
-        value: hubInfo['players_joined'].toString(),
+        value: hubInfo.players_joined.toString(),
         inline: true
       }, {
         name: 'Min/Max Elolevel',
-        value: hubInfo['min_skill_level'] + ' / ' + hubInfo['max_skill_level'],
+        value: `${hubInfo.min_skill_level} / ${hubInfo.max_skill_level}`,
         inline: true
       }, {
         name: 'Permissions',
-        value: hubInfo['join_permission'],
+        value: hubInfo.join_permission,
         inline: true
       }, {
         name: 'FaceIT Link',
-        value: '[Link to FaceIT Hub](' + hubInfo['faceit_url'].replaceAll('{lang}', 'en') + ')',
+        value: `[Link to FaceIT Hub](${hubInfo.faceit_url.replaceAll('{ lang }', 'en')})`,
         inline: true
       })
       .setThumbnail(hubInfo.avatar)
 
-    var lDesc = []
-    for (var i = 0; i < 9; i++) {
-      var played = hubLeaderboard.items[i].played
-      var winrate = hubLeaderboard.items[i]['win_rate']
-      var lvl = hubLeaderboard.items[i]['points']
-      var nick = hubLeaderboard.items[i].player.nickname
+    let lDesc = []
+    for (let i = 0; i < 9; i++) {
+      let played = hubLeaderboard.items[i].played
+      let winrate = hubLeaderboard.items[i].win_rate
+      let lvl = hubLeaderboard.items[i].points
+      let nick = hubLeaderboard.items[i].player.nickname
       lDesc.push(i + 1 + '. ***' + nick + '***: ' + 'Points: ' + lvl + ' | ' + 'Played Matches: ' + played + ' | ' + 'Winrate: ' + winrate + '\n')
     }
 
@@ -563,24 +511,22 @@ async function fHub(name) {
       .setTimestamp()
 
     return [errembed]
-
   }
 }
 
-async function fTeam(name) {
+const fTeam = async name => {
   try {
-    var teamSearch = await fr.teamSearch(name)
-    var teamInfo = await fr.teamInfo(teamSearch.items[0]['team_id'])
+    const teamSearch = await teamSearch(name),
+      teamInfo = await teamInfo(teamSearch.items[0].team_id)
 
     const embed = new MessageEmbed()
       .setColor('#FF5500')
       .setTitle('Team ' + teamSearch.items[0]['name'])
-      .setDescription(teamInfo['description'] + '\n [Link to FaceIT](' + teamInfo['faceit_url'].replaceAll('{lang}', 'en') + ')')
+      .setDescription(`${teamInfo.description}\n[Link to FaceIT](${teamInfo['faceit_url'].replaceAll('{lang}', 'en')})`)
       .setThumbnail(teamInfo.avatar)
 
-    for (var i = 0; i < teamInfo.members.length; i++) {
-      embed.addField(name = countryCodeEmoji(teamInfo.members[i].country) + ' ' + teamInfo.members[i].nickname, value = '[Link to Profile](' + (teamInfo.members[i]['faceit_url']).replace('{lang}', 'en') + ')'.toString())
-    }
+    for (let i = 0; i < teamInfo.members.length; i++)
+      embed.addField(`${countryCodeEmoji(teamInfo.members[i].country)}  ${teamInfo.members[i].nickname}`, value = `[Link to Profile](${teamInfo.members[i].faceit_url.replace('{lang}', 'en')})`)
 
     return embed
   } catch (e) {
