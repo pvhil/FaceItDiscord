438cf6be3baac45f2994624a32b54eab9c628e1b
JustDams <ddw_github@de-weerd.name>
2022-06-02 22:56:35 +0200

    :art: format ranking

---
+++ commands/ranking.js
@@ -0,0 +1,16 @@
+const { fRanking } = require('../embedFinisher')
+const { getInteractionOption } = require('../utils/interaction')
+const { postCommandToStatcord } = require('../utils/statcord')
+
+module.exports = async interaction => {
+  await interaction.deferReply()
+  const region = getInteractionOption(interaction, 'region')
+  const country = getInteractionOption(interaction, 'country')
+  const resp = await fRanking(region, country)
+
+  postCommandToStatcord(interaction)
+
+  interaction.editReply({
+    embeds: [resp]
+  })
+}
---
+++ commands/save.js
@@ -0,0 +1,52 @@
+const { getInteractionOption } = require('../utils/interaction')
+
+module.exports = async interaction => {
+  await interaction.deferReply()
+  const name = getInteractionOption(interaction, 'faceit')
+  const uId = interaction.user.id
+
+  try {
+    const valid = await fr.nickStats(name)
+    const level = valid.games.csgo['skill_level']
+
+    console.log(level)
+
+    pgclient.query('INSERT INTO stats(discord,faceit) VALUES ($1,$2) ON CONFLICT ON CONSTRAINT stats_pkey DO UPDATE SET faceit=EXCLUDED.faceit;', [uId, name],
+      (err, res) => {
+        const errembed = new MessageEmbed()
+          .setColor('#FF5500')
+          .setTitle('Saved your FaceIT Name')
+          .setDescription('You will gain Level-Specific Roles when the Rolesystem is activated.\nOther Users also can Right-Click on you to see Statistics')
+          .setTimestamp()
+
+        interaction.editReply({
+          embeds: [errembed],
+          ephemeral: true
+        })
+        postCommandToStatcord(interaction)
+      })
+
+  } catch (err) {
+    const errembed = new MessageEmbed()
+      .setColor('#ff0000')
+      .setTitle('This FaceIT-Name is invalid!')
+      .setDescription('Remember that the FaceIT Name is case-sensetive!')
+      .setTimestamp()
+
+    interaction.editReply({
+      embeds: [errembed],
+      ephemeral: true
+    })
+  }
+
+  const guildid = interaction.guild.id
+
+  try {
+    const guildInfo = await syncQuery(`SELECT * FROM levelrole WHERE discordid=${guildid}`)
+    if (guildInfo.rows.length === 1) {
+      await interaction.member.roles.add(guildInfo.rows[0]['level' + level])
+    }
+  } catch (e) {
+    console.log('no rolesystem')
+  }
+}
