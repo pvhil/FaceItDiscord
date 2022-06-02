911fb11513fea44cd64056b23d2d6e304b14564c
JustDams <ddw_github@de-weerd.name>
2022-06-02 20:37:35 +0200

    :art: linter fix

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const dotenv = require('dotenv').config()
+require('dotenv').config()
 const {
-    ShardingManager
-} = require('discord.js');
-const Statcord = require("statcord.js");
+  ShardingManager
+} = require('discord.js')
+const Statcord = require('statcord.js')
 
 const manager = new ShardingManager('./bot.js', {
-    token: (process.env.BOTTOKEN).toString()
-});
+  token: (process.env.BOTTOKEN).toString()
+})
 
 const statcord = new Statcord.ShardingClient({
-    key: process.env.STATTOKEN,
-    manager,
-    postCpuStatistics: true,
-    postMemStatistics: true,
-    postNetworkStatistics: true,
-    autopost: true
-});
+  key: process.env.STATTOKEN,
+  manager,
+  postCpuStatistics: true,
+  postMemStatistics: true,
+  postNetworkStatistics: true,
+  autopost: true
+})
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`))
+manager.spawn()
 
-manager.spawn();
+statcord.on('autopost-start', () => {
+  console.log('Started autopost')
+})
 
-statcord.on("autopost-start", () => {
-    console.log("Started autopost");
-});
-
-statcord.on("post", status => {
-    if (!status) console.log("Successful post");
-    else console.error(status);
-});
+statcord.on('post', status => {
+  if (!status) console.log('Successful post')
+  else console.error(status)
+})
