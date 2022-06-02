4d269241c5265d68b877c048afa2af72e9ab1b84
JustDams <ddw_github@de-weerd.name>
2022-06-02 22:24:08 +0200

    :recycle: refactor faceitRequests

--- faceitRequests.js
+++ faceitRequests.js
@@ -1,407 +1,83 @@
-const axios = require('axios');
-const dotenv = require('dotenv').config()
-
+const axios = require('axios')
+require('dotenv').config()
 
 const ft = process.env.FACEITTOKEN
 const topggtoken = process.env.TOPTOKEN
 
-// search player
-function searchPlayer(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/search/players?nickname=" + encodeURIComponent(x) + "&game=csgo&offset=0&limit=1", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-        })
-    } catch (err) {
-      reject("error")
+const defaultQuery = url => new Promise((resolve, reject) => {
+  axios.get(url, {
+    headers: {
+      'accept': 'application/json',
+      'Authorization': `Bearer ${ft}`
     }
   })
-}
+    .then((response) => resolve(response.data))
+    .catch((error) => {
+      reject('error')
+      console.log(error.message, url)
+    })
+})
+
+// search player
+const searchPlayer = x => defaultQuery(`https://open.faceit.com/data/v4/search/players?nickname=${encodeURIComponent(x)}&game=csgo&offset=0&limit=1`)
 
 // return match need matchid
-async function detailedMatch(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/matches/" + encodeURIComponent(x) + "/stats", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const detailedMatch = x => defaultQuery(`https://open.faceit.com/data/v4/matches/${encodeURIComponent(x)}/stats`)
 
 //search hub need hub name
-async function searchHub(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/search/hubs?name=" + encodeURIComponent(x) + "&offset=0&limit=1", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const searchHub = x => defaultQuery(`https://open.faceit.com/data/v4/search/hubs?name=${encodeURIComponent(x)}&offset=0&limit=1`)
 
 // hub info need id?
-async function hubInfo(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/hubs/" + encodeURIComponent(x), {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const hubInfo = x => defaultQuery(`https://open.faceit.com/data/v4/hubs/${encodeURIComponent(x)}`)
 
 // hub leaderboard hub id?
-async function hubLeaderboard(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/leaderboards/hubs/" + encodeURIComponent(x) + "/general?offset=0&limit=10", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const hubLeaderboard = x => defaultQuery(`https://open.faceit.com/data/v4/leaderboards/hubs/${encodeURIComponent(x)}/general?offset=0&limit=10`)
 
 //last 20 games playerid
-async function vOneRequest(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://api.faceit.com/stats/api/v1/stats/time/users/" + encodeURIComponent(x) + "/games/csgo", {
-
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const vOneRequest = x => defaultQuery(`https://api.faceit.com/stats/api/v1/stats/time/users/${encodeURIComponent(x)}/games/csgo`)
 
 // needed for last games needs number and id
-async function historyReq(x, numb) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/players/" + encodeURIComponent(x) + "/history?game=csgo&offset=0&limit=" + numb, {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const historyReq = x => defaultQuery(`https://open.faceit.com/data/v4/players/${encodeURIComponent(x)}/history?game=csgo&offset=0&limit=20`)
 
 //normal stats need id
-async function idStats(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/players/" + x + "/stats/csgo", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const idStats = x => defaultQuery(`https://open.faceit.com/data/v4/players/${encodeURIComponent(x)}/stats/csgo`)
 
 //normal stats need id
-async function idStatsCheck(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/players/" + x, {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const idStatsCheck = x => defaultQuery(`https://open.faceit.com/data/v4/players/${encodeURIComponent(x)}`)
 
 //stats with nickname
-async function nickStats(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/players?nickname=" + encodeURIComponent(x), {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const nickStats = x => defaultQuery(`https://open.faceit.com/data/v4/players?nickname=${encodeURIComponent(x)}`)
 
 //other games id and gameid
-async function otherGames(x, game) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/players/" + encodeURIComponent(x) + "/stats/" + game, {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
-
+const otherGames = (x, game) => defaultQuery(`https://open.faceit.com/data/v4/players/${encodeURIComponent(x)}/stats/${game}`)
 
 //player rank in region, needs id and region
-async function plRankRegion(x, reg) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/rankings/games/csgo/regions/" + reg + "/players/" + encodeURIComponent(x) + "?limit=1", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const plRankRegion = (x, reg) => defaultQuery(`https://open.faceit.com/data/v4/rankings/games/csgo/regions/${reg}/players/${encodeURIComponent(x)}?limit=1`)
 
 //player rank in country, needs id and country and region
-async function plRankCountry(x, country, reg) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/rankings/games/csgo/regions/" + reg + "/players/" + encodeURIComponent(x) + "?country=" + country + "&limit=1", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const plRankCountry = (x, country, reg) => defaultQuery(`https://open.faceit.com/data/v4/rankings/games/csgo/regions/${reg}/players/${encodeURIComponent(x)}?country=${country}&limit=1`)
 
 //region rankings need region
-async function rankRegion(reg) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/rankings/games/csgo/regions/" + reg + "?offset=0&limit=15", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const rankRegion = reg => defaultQuery(`https://open.faceit.com/data/v4/rankings/games/csgo/regions/${reg}?offset=0&limit=15`)
 
 //ranking country needs region and country
-async function rankCountry(country, reg) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/rankings/games/csgo/regions/" + reg + "?country=" + country + "&offset=0&limit=15", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const rankCountry = (country, reg) => defaultQuery(`https://open.faceit.com/data/v4/rankings/games/csgo/regions/${reg}?country=${country}&offset=0&limit=15`)
 
 // team search needs team
-async function teamSearch(team) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/search/teams?nickname=" + encodeURIComponent(team) + "&offset=0&limit=1", {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const teamSearch = team => defaultQuery(`https://open.faceit.com/data/v4/search/teams?nickname=${encodeURIComponent(team)}&offset=0&limit=1`)
 
 // team info needs teamid
-async function teamInfo(x) {
-  return new Promise((resolve, reject) => {
-    try {
-      axios.get("https://open.faceit.com/data/v4/teams/" + encodeURIComponent(x), {
-          headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + ft
-          }
-        })
-        .then(function (response) {
-          resolve(response.data)
-        })
-        .catch(function (error) {
-          reject("error")
-          console.log(error);
-        })
-    } catch (err) {
-      return "error"
-    }
-  })
-}
+const teamInfo = x => defaultQuery(`https://open.faceit.com/data/v4/teams/${encodeURIComponent(x)}`)
 
 // extra: vote check
 async function topggcheck(x) {
   return new Promise((resolve, reject) => {
     try {
-      axios.get("https://top.gg/api/bots/770312130037153813/check?userId="+x, {
+      axios.get("https://top.gg/api/bots/770312130037153813/check?userId=" + x, {
         headers: {
           'Authorization': topggtoken
         }
-        })
+      })
         .then(function (response) {
           resolve(response.data)
         })
