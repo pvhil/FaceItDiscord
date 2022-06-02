65295f3a76b57d9e9be8dc36f2e4c4900a5b06fd
JustDams <ddw_github@de-weerd.name>
2022-06-02 23:00:19 +0200

    :art: linter fix + format

--- rolesystem/index.js
+++ rolesystem/index.js
@@ -1,17 +1,17 @@
-const dotenv = require('dotenv').config()
-const discordToken = process.env.BOTTOKEN
+require('dotenv').config()
+
 const {
-    ShardingManager
-} = require('discord.js');
+  ShardingManager
+} = require('discord.js')
 
 const manager = new ShardingManager('./rolesystem.js', {
-    token: discordToken
-});
+  token: process.env.BOTTOKEN
+})
 
 manager.on('shardCreate', shard => {
-    shard.on("ready", () => {
-        shard.send({type: "shardId", data: {shardId: shard.id}});
-    });
-});
+  shard.on('ready', () => {
+    shard.send({type: 'shardId', data: {shardId: shard.id}})
+  })
+})
 
-manager.spawn();
+manager.spawn()
